1f71b00e350d1715721b76a135a05ea4f12a19a5
Yuri Tagomori <goncalvessyuri@gmail.com>
2023-08-07 15:02:43 -0300

    fixing interaction

--- main.js
+++ main.js
@@ -3,32 +3,40 @@ const guessAgain = document.querySelector(".guess-again");
 const btnTry = document.querySelector("#btn-try");
 const btnTryAgain = document.querySelector("#btn-tryagain");
 
-const randomNumber = Math.round(Math.random() * 10);
-let xAttempts = 1;
+let randomNumber;
+let xAttempts;
+
+function startNewGame() {
+    randomNumber = Math.round(Math.random() * 10);
+    xAttempts = 1;
+}
 
 function handleTryClick(event) {
     event.preventDefault();
 
     const inputNumber = document.querySelector("#inputNumber");
 
-    if (Number(inputNumber.value) == randomNumber) {
+    if (Number(inputNumber.value) === randomNumber) {
         toggleScreen();
-        guess.querySelector("h2").innerText = `Got it in ${xAttempts} tries!`
-    };
-    
-    inputNumber.value = "";
-    xAttempts++;
+        const attempts = guessAgain.querySelector("h2");
+        attempts.innerText = `Got it in ${xAttempts} tries!`;
+    } else {
+        inputNumber.value = "";
+        xAttempts++;
+    }
 }
 
 function handleResetClick() {
     toggleScreen();
-    xAttempts = 1;
-};
+    startNewGame();
+}
 
 btnTry.addEventListener('click', handleTryClick);
 btnTryAgain.addEventListener('click', handleResetClick);
 
 function toggleScreen() {
-    guess.classList.toggle("hide")
-    guessAgain.classList.toggle("hide")
-};
+    guess.classList.toggle("hide");
+    guessAgain.classList.toggle("hide");
+}
+
+startNewGame();
